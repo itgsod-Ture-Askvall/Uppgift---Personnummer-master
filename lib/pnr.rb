605f0be6d22c83b9406f87f6420bee7301e63d2f
itgsod-Ture-Askvall <ture.askvall@Administrators-MacBook-Pro-97.local>
2015-05-28 16:14:21 +0200

    JAG BÖRJADE OM FÖR JAG HATAR DEN HÄR

---
+++ lib/pnr.rb
@@ -0,0 +1,27 @@
+def valid_pnr?(pnr:)
+
+
+  summa=0
+  pnr = pnr.to_s.split(//)
+  i= 1
+  pnr_integer = pnr.map(&:to_i)
+  p pnr_integer
+
+  until i==9
+    pnr_integer.each do |nummer|
+      if i.%(2).zero?
+        nummeren = nummer * 2
+        nummeren =
+        nummeren.to_s.chars.map(&:to_i)
+        nummeren.each do |tal|
+          summa = summa + tal
+          puts summa
+
+        end
+      end
+    end
+  end
+end
+
+p valid_pnr?(pnr:'19971223-3270')
+
